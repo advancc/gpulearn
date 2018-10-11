7fef83fb190cc852f3cd77fafabead0c12d55b09
advancc <syumening@vip.qq.com>
2018-10-11 14:37:48 +0800

    Update simu.cpp

--- simu.cpp
+++ simu.cpp
@@ -11,7 +11,7 @@ void generateRandomInit();
 
 int main()
 {
-    int total_num = 100;
+    	int total_num = 100;
 	int max_n = 10;
 	int max_time = 10;
 	size_t nBytes = total_num * max_n * sizeof(double);
@@ -35,16 +35,16 @@ int main()
 	}
 	double *h_res = (double*)malloc(nBytes);
 
-    LARGE_INTEGER t1,t2,tc;
-    QueryPerformanceFrequency(&tc);
-    QueryPerformanceCounter(&t1);
+	LARGE_INTEGER t1,t2,tc;
+	QueryPerformanceFrequency(&tc);
+	QueryPerformanceCounter(&t1);
 
-    CDF_Sampling(pmt, hittime, h_res, total_num);
+	CDF_Sampling(pmt, hittime, h_res, total_num);
 
-    QueryPerformanceCounter(&t2);
-    printf("CPU Use Time:%f us\n",1000000*(t2.QuadPart - t1.QuadPart)*1.0/tc.QuadPart);
+	QueryPerformanceCounter(&t2);
+	printf("CPU Use Time:%f us\n",1000000*(t2.QuadPart - t1.QuadPart)*1.0/tc.QuadPart);
 
-    for (int i = 0;i < total_num;i++)
+	for (int i = 0;i < total_num;i++)
 	{
 		for (int j = 0;j < max_time;j++)
 		{
@@ -58,11 +58,11 @@ int main()
 	free(h_res);
 
 
-    return 0;
+	return 0;
 }
 double generateRandom()
 {
-    return (double)rand()/RAND_MAX;
+	return (double)rand()/RAND_MAX;
 }
 void generateRandomInit()
 {
@@ -71,9 +71,9 @@ void generateRandomInit()
 void CDF_Sampling(double *pmt, double *hittime, double *result, int numElements)
 {
 
-    for(int i=0;i < numElements;i++)
-    {
-        generateRandomInit();
+	for(int i=0;i < numElements;i++)
+	{
+		generateRandomInit();
 		double prob = generateRandom();
 		double sum = 0;
 		int n = 0;
@@ -103,5 +103,5 @@ void CDF_Sampling(double *pmt, double *hittime, double *result, int numElements)
 			}
 
 		}
-    }
+	}
 }
