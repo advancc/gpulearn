86f7444e58492a62099fe61d4cd8231cc957394f
advancc <syumening@vip.qq.com>
2018-11-05 15:09:19 +0800

    Add files via upload

---
+++ simu_plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+filename = 'logger.log'
+with open(filename) as file_object:
+    index=0
+    x_list = []
+    gpu_list = []
+    cpu_list = []
+    for line in file_object:
+        temp = line.rstrip("\n").split(":")
+        # print(temp)
+        if index == 0:
+            x_list.append(float(temp[3]))
+        elif index == 1:
+            gpu_list.append(float(temp[3]))
+        elif index == 2:
+            cpu_list.append(float(temp[3]))
+        index = (index+1)%3
+    # print(x_list)
+    # print(gpu_list)
+    # print(cpu_list)
+    plt.plot(x_list,gpu_list,label='gpu')
+    plt.plot(x_list,cpu_list,label='cpu')
+    plt.title("GPU vs CPU")
+    plt.xlabel('parallellism thread number')
+    plt.ylabel("time(ms)")
+    plt.legend()#显示图例
+    plt.show()
+
